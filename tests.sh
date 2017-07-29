7cf0280e73b439d339038aa42f28d8801c69169c
Zac Sturgess <zac.sturgess@wearetwogether.com>
2017-07-29 22:53:19 +0100

    Map out tests

--- tests.sh
+++ tests.sh
@@ -6,6 +6,69 @@ testErrorsWhenRunWithNoOptions()
   assertFalse 'Returns error code when run with no options' ./workbench.sh
 }
 
+testRunWithNoOptionsShowsHelp()
+{
+  assertTrue 'Shows help when run with no options' '[[ "`./workbench.sh`" == *"help message"* ]]'
+}
+
+testErrorsWhenPassedUnrecognisedOption()
+{
+  assertFalse 'Returns error code when run with unrecognised options' './workbench.sh -z bad'
+  assertTrue 'Shows error message when passed unrecognised option' '[[ "`./workbench.sh -z bad`" == *"Unrecognised option"* ]]' 
+  assertTrue 'Shows help when run with unrecognised options' '[[ "`./workbench.sh -z bad`" == *"help message"* ]]'
+}
+
+testErrorsWhenPassedBadConfigFile()
+{
+  assertFalse 'Returns error code when run with config file that does not exist' './workbench.sh -c bad'
+  assertTrue 'Shows error message when passed config file that does not exist' '[[ "`./workbench.sh -c bad`" == *"Config file provided does not exist"* ]]' 
+}
+
+testPerformsFirstTimeSetup()
+{
+  true
+}
+
+testPerformsFirstTimeSetupWithCustomConfigLocation()
+{
+  true
+}
+
+testSkipsFirstTimeSetupWhenConfigExists()
+{
+  true
+}
+
+testUsesCustomConfigWhenProvided()
+{
+  true
+}
+
+testHandlesFailureToGrabAccessTokenFromSFDC()
+{
+  true
+}
+
+testHandlesQueries()
+{
+  true
+}
+
+testHandlesDescribes()
+{
+  true
+}
+
+testHandlesQueriesInXML()
+{
+  true
+}
+
+testHandlesDecribesInXML()
+{
+  true
+}
+
 if [ ! -f shunit2 ]; then
   echo 'Fetching testing library...'
   curl 'https://raw.githubusercontent.com/kward/shunit2/source/2.1.6/src/shunit2' -o shunit2
@@ -13,6 +76,6 @@ if [ ! -f shunit2 ]; then
   echo
 fi
 
-echo 'Running tests'
+echo 'Running tests...'
 echo
 . shunit2
